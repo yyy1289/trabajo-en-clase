e298a6c6eb7e12ad3832a0032938fc5afd21368f
yerson <yrvasquez1@misena.edu.co>
2019-11-01 20:50:44 -0500

    version inicial

---
+++ proyectoprueba/src/proyectoprueba/Proyectoprueba.java
@@ -0,0 +1,18 @@
+package proyectoprueba;
+
+import java.util.Scanner;
+
+public class Proyectoprueba {
+
+   
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner opc = new Scanner (System.in);
+    System.out.println("Digite su nombre");
+    String nombre = opc.nextLine();
+    
+    
+    }
+    
+}
